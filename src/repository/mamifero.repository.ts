2e115b5fa622e210d9487df1ce9aa4cb5aff1520
Joao Marcos <joaomoura002@outlook.com>
2023-05-25 01:43:46 -0300

    Create funcionando :).

--- src/repository/mamifero.repository.ts
+++ src/repository/mamifero.repository.ts
@@ -3,6 +3,12 @@ import { prisma } from "../services/prisma";
 export const createMamifero = async(data:any) => {
     const mamifero = await prisma.mamiferos.create({
         data,
+        select: {
+            nome: true,
+            descricao:true,
+            peso: true,
+            extincao: true
+        }
     });
-    return mamifero;
+    return data;
 }
